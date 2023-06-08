7ed60fbc59eb48e58b38b49dcac1e4bbe7ef33e3
mktcode <kontakt@markus-kottlaender.de>
2023-06-08 14:47:29 +0200

    initial implementation

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,76 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as ignore from 'ignore';
+import * as fs from 'fs';
+import * as path from 'path';
+import * as cp from 'child_process';
+
+async function getGitUsername(): Promise<string | undefined> {
+  return new Promise((resolve) => {
+    cp.exec('git config user.name', (error, stdout) => {
+      if (error) {
+        resolve(undefined);
+      } else {
+        resolve(stdout.trim());
+      }
+    });
+  });
+}
+
+async function watchFileChanges(context: vscode.ExtensionContext) {
+  const gitignorePath = path.join(vscode.workspace.rootPath || '', '.gitignore');
+  const gitignore = ignore.default();
+  if (fs.existsSync(gitignorePath)) {
+    const gitignoreContent = fs.readFileSync(gitignorePath).toString();
+    gitignore.add(gitignoreContent);
+  }
+
+  const fileSystemWatcher = vscode.workspace.createFileSystemWatcher('**/*.*');
+  context.subscriptions.push(fileSystemWatcher);
+
+  fileSystemWatcher.onDidChange(async (uri) => {
+    const relativePath = vscode.workspace.asRelativePath(uri.fsPath);
+
+    if (gitignore.ignores(relativePath)) {
+      return;
+    }
+
+    const activeDocument = vscode.window.activeTextEditor?.document;
+    if (activeDocument && activeDocument.uri.fsPath === uri.fsPath) {
+      return;
+    }
+
+    const username = await getGitUsername();
+    if (!username) {
+      return;
+    }
+
+    const fileContent = fs.readFileSync(uri.fsPath).toString();
+    const regex = new RegExp(`@${username}:`, 'g');
+    let match: RegExpExecArray | null;
+
+    while ((match = regex.exec(fileContent)) !== null) {
+      const position = new vscode.Position(fileContent.substr(0, match.index).split('\n').length - 1, match.index);
+      const range = new vscode.Range(position, position.translate(0, username.length + 1));
+      vscode.window.showInformationMessage(`You were mentioned in ${uri.fsPath}`, 'Go to mention').then((action) => {
+        if (action === 'Go to mention') {
+          vscode.workspace.openTextDocument(uri).then((doc) => {
+            vscode.window.showTextDocument(doc).then((editor) => {
+              editor.selection = new vscode.Selection(range.start, range.end);
+              editor.revealRange(range);
+            });
+          });
+        }
+      });
+    }
+  });
+}
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "notify-teammates" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('notify-teammates.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Notify Teammates!');
-	});
-
-	context.subscriptions.push(disposable);
+  watchFileChanges(context);
 }
 
 // This method is called when your extension is deactivated
